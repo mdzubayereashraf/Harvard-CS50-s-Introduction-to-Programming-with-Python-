4921f70618cba261e8056a6c5df7077aeee456c7
mdzubayereashraf <mdzubayereashraf@gmail.com>
2026-05-25 11:50:33 +0600

    input codes updated

--- My python journey/Input exanples.py
+++ My python journey/Input exanples.py
@@ -1,7 +1,23 @@
+# Asking for user input
+# name = input( "Enter your name :")
+# name = name.strip() # I used this to skip white space
+# name = name.title() # I used this method to capitilize of every word's fisrt character
+# age = int(input( " Enter your age:"))
+# mark = int(input("Enter your mark:"))
 
-name = input( "Enter your name :")
-age = int(input( " Enter your age:"))
-mark = int(input("Enter your mark:"))
-print("Your name is", name)
-print("Your age is", age)
-print("Your name is", mark)
+
+# #casual method of priting input function
+# print("Your name is", name)
+# print("Your age is", age)
+# print("Your name is", mark)
+
+# now I'm making codes more advanced and organised
+name = input("Enter your name, please. ").strip().title()
+age = int(input("Enter your age, please. "))
+mark = int(input("Enter your mark, please. "))
+
+# Using advanced print formula
+print(f"Your name is : {name}")
+print(f"Your age is : {age}")
+print(f"Your mark is : {mark}")
+print("Thank you for tunning us")
