45d3a7f7ecf270ae60b840d0f7429faf4a6af36f
mdzubayereashraf <mdzubayereashraf@gmail.com>
2026-06-06 22:34:25 +0600

    added string method

---
+++ My python journey/Basic Codes/string formating.py
@@ -0,0 +1,11 @@
+#String Formating in Python
+name = "John"
+age = 30    
+#Using f-string
+print(f"My name is {name} and I am {age} years old.")   
+#Using format method
+print("My name is {} and I am {} years old.".format(name, age))
+#Using % operator
+print("My name is %s and I am %d years old." % (name, age))     
+#Using concatenation
+print("My name is " + name + " and I am " + str(age) + " years old.")
