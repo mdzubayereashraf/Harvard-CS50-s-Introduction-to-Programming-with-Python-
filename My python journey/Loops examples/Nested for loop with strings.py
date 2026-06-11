c4c2ddf1f5b94be27dd7e2faa9f96a8a4d899837
mdzubayereashraf <mdzubayereashraf@gmail.com>
2026-06-11 08:25:28 +0600

    added nested for loops examples

---
+++ My python journey/Loops examples/Nested for loop with strings.py
@@ -0,0 +1,5 @@
+Shirts = ["Red Shirt", "Blue Shirt", "Green Shirt"]
+Pants = ["Jeans", "Trousers",]
+for shirt in Shirts:
+    for pant in Pants:
+        print(f"{shirt} goes well with {pant}") 
---
+++ My python journey/Loops examples/Nested loops with integer.py
@@ -0,0 +1,7 @@
+for i in range (1,4):
+    for j in range (1,3):
+        print(f"Outter loop: i = {i}, Inner loop: j = {j}")
+
+for i in range (1,4):
+    for j in range (1,3):
+        print(f"({i},{j})")
